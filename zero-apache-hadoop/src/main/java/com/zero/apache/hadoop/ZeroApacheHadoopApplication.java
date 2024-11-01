c509b56cc1d2be6079ac4b17032279a548c5313b
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-01 08:48:24 +0800

    feat: springboot 集成 hadoop

---
+++ zero-apache-hadoop/src/main/java/com/zero/apache/hadoop/ZeroApacheHadoopApplication.java
@@ -0,0 +1,13 @@
+package com.zero.apache.hadoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroApacheHadoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroApacheHadoopApplication.class, args);
+    }
+
+}
---
+++ zero-apache-hadoop/src/test/java/com/zero/apache/hadoop/ZeroApacheHadoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.apache.hadoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroApacheHadoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
